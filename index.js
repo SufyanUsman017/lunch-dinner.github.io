adc36bf2117c8ee7abaf3ba63ddb240f441010b6
SufyanUsman017 <162179435+SufyanUsman017@users.noreply.github.com>
2024-11-14 13:09:09 +0500

    time-lunch-dinner

---
+++ index.js
@@ -0,0 +1,106 @@
+// if else and if else condition
+
+// let names = "shaban";
+// if(names== "sufyan"){
+//     console.log("Best friend");
+// }
+
+// else if (names=="zohaib"){
+//     console.log("just a friend");
+
+// }
+// else if (names == "shaban"){
+//     console.log("Normal friend");
+
+// }
+
+// else{
+//     console.log("Not a friend");
+
+// }
+
+// Mathematics Formula V S T
+
+// let car = prompt("What do you want to find 1.v 2.s 3.t");
+
+// if(car == "v"){
+//    let s = prompt("Enter the value of s");
+//    let t = prompt("Enter the value of t");
+//    const v = s / t ;
+//    alert(`The Value of Velocity is ${v} m/sec `)
+   
+// }
+
+// else if(car=="s"){
+//    let v = prompt("Enter the value of v")
+//    let t = prompt("Enter the value of t")
+//    const s = v * t ;
+//    alert(`The value of distance ${s}m`) 
+
+// }
+
+// else if(car == "t"){
+//    let s = prompt("Enter the value of s")
+//    let v = prompt("Enter the value of
+//     console.log(`The Value of time is ${t}sec`);
+
+// } v")
+//     const t = s / v;
+
+// else{
+//     alert("This is not find")
+// }
+
+/*****************************Nested if else********************************************** */
+// let time = prompt("Enter a time");
+// if(time<=6){
+//    alert("Late Night");
+// }
+
+// else if(time<=12){
+//    alert("Morning Time");
+// }
+// else if(time<=17){
+//    alert("Evening Time");
+// }
+// else if(time<=24){
+//    alert("Night Time");
+// }
+// else{
+//    alert("Time is not Found")
+// }
+/*****************************Nested if else********************************************** */
+/*****************************Nested condition********************************************** */
+let times = prompt("Enter a time");
+if(times<=6){
+  let user = prompt("1.McDonald 2.KFC");
+  if(user==1){
+      alert("This product is better than KFC");
+  }
+
+ else if(user==2){
+   alert("This product is better than McDonald");
+}
+else{
+   alert("Gate loss");
+}
+let times = prompt("Enter a time");
+if(times<=12){
+      // alert("Morning Time");
+      let user = prompt("1.Subah ka nashta 2.nahsta nhi krenge")
+      if(user==1){
+         alert("Dilpasand chlenge");
+      }
+      else if(user==2){
+         alert("Mood nhi hai yar")
+      }
+      else{
+         alert("Mujh krna hi nhi hai")
+      }
+   }
+}
+
+
+
+
+/*****************************Nested condition********************************************** */
